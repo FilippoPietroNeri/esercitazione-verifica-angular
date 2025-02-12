8df81b89981bd0dd6ab8ce3d3555590226e48ca4
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2025-02-12 13:55:20 +0000

    Finito

--- src/app/third/third.component.ts
+++ src/app/third/third.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FourthComponent } from '../fourth/fourth.component';
 
 @Component({
   selector: 'app-third',
-  imports: [],
+  imports: [FourthComponent],
   templateUrl: './third.component.html',
   styleUrl: './third.component.css',
   standalone: true,
 })
 export class ThirdComponent {
-
+  name = 'Third';
 }
